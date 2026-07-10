133cdcde12daa98d3f213a25ae789a4359219b06
Sayan De <04sayan96@gmail.com>
2026-07-10 23:09:44 +0530

    2nd one compleated

--- courses/02-redux-rtk-query/project/src/components/CounterView.tsx
+++ courses/02-redux-rtk-query/project/src/components/CounterView.tsx
@@ -1,4 +1,25 @@
-/** Stub: Complete Challenge 03 (Reading and Dispatching) per README. */
-export default function CounterView() {
-  return <div id="counter-view">Complete Challenge 03 per README.</div>
+import { increment, decrement } from "../store/slices/counterSlice";
+
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+
+const CounterView = () => {
+  const count = useAppSelector((state) => state.counter);
+      const dispatch = useAppDispatch();
+
+  return (
+    <div>
+      <h1 data-testid="counter-value">{count}</h1>
+
+      <button data-testid="increment-btn"
+     onClick={() => dispatch(increment())}>
+        Increment </button>
+
+     <button
+        data-testid="decrement-btn"
+        onClick={() => dispatch(decrement())}>
+        Decrement </button>
+    </div>
+  );
 }
+
+export default CounterView;
